23907b3d7edca39a861f17875f9de7e35405447d
calsilva <ka.renner@hotmail.com>
2023-03-19 15:36:51 -0300

    [new]: create test assumptions

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,14 @@
 
 describe("fizzbuzz", () => {
+    const cases: Array<[number, string]> = [
+        [17, '17'],
+        [33, 'Fizz'],
+        [25, 'Buzz.'],
+        [45, 'FizzBuzz.'],
+        [30, 'FizzBuzz.'],
+    ]
 
-
+    describe('given a number between (including) 1 and 100', () => {
+        it.todo('should replace %s per %s')
+    })
 });
