7bdb5cc02733393ead098234c8b69ba39258923b
calsilva <ka.renner@hotmail.com>
2023-03-19 13:20:26 -0300

    [new]: create test assumptions

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,4 +1,19 @@
 
 describe('palindrome checker', () => {
+    const cases = [
+        ['mom', true],
+        [' mom', true],
+        [' MoM ', true],
+        ['Was It A Rat I Saw ', true],
+        [' this is not a palindrome', false],
+        ['palindrome', false]
+    ]
 
+    describe('if the given string is a palindrome', () => {
+        it.todo('should return true')
+    })
+
+    describe('otherwise', () => {
+        it.todo('should return false')
+    })
 })
