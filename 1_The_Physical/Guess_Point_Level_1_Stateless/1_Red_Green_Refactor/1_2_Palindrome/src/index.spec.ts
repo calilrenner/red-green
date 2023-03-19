df6b0af7dfded7db0fd7fde628ecb22793b8dc73
calsilva <ka.renner@hotmail.com>
2023-03-18 22:37:03 -0300

    Revert "test(WARMUP): test assumptions"
    This reverts commit 46649fce8079b9330ff0330d8cca8388c742d8e4.

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,13 +1,4 @@
 
 describe('palindrome checker', () => {
-    describe('if the given string is a palindrome', () => {
-        it.todo('should return true if a single word has been passed')
-        it.todo('should return true if a single word mixed with upper/lower case has been passed')
-        it.todo('should return true if a single word with extra spaces has been passed')
-        it.todo('should return true if a phrase has been passed')
-    })
 
-    describe('otherwise', () => {
-        it.todo('should throw NotAPalindromeException')
-    })
 })
