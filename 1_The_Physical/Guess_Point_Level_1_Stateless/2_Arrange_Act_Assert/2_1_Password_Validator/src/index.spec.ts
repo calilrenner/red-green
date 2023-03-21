719761a245001bad8421d82a2b54d4515ddb24ac
calsilva <ka.renner@hotmail.com>
2023-03-20 22:11:45 -0300

    [new]: return result:false and errors['passwordInvalidLength'] if password <= 4

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -6,7 +6,26 @@ describe('password validator', () => {
         - result: boolean;
         - errors: []
         `, () => {
-            expect(passwordValidator('abc123')).toStrictEqual({result: true, errors: []})
+            expect(passwordValidator('12345')).toStrictEqual({result: true, errors: []})
+        })
+    })
+
+    describe('otherwise', () => {
+        describe('invalid password', () => {
+            const password = '1234';
+            describe('password has up to 4 characters long', () => {
+                it(`should return:
+                - result: false,
+                - errors: [passwordInvalidLength]
+                `, () => {
+                    const expected = {
+                        result: false,
+                        errors: ['passwordInvalidLength']
+                    }
+
+                    expect(passwordValidator(password)).toStrictEqual(expected)
+                })
+            })
         })
     })
 })
--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
@@ -1,3 +1,7 @@
 export function passwordValidator(password: string) {
+    if (password.length <= 4) {
+        return {result: false, errors: ['passwordInvalidLength']}        
+    }
+
     return {result: true, errors: []}
 } 
