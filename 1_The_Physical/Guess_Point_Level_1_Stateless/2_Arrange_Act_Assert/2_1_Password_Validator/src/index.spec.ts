789ef8664feca5ca5e72377d9760d70220d83125
calsilva <ka.renner@hotmail.com>
2023-03-20 21:45:11 -0300

    [new]: ensure passwordValidator returns result:boolean and errors:Array

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -1,5 +1,13 @@
+import { passwordValidator } from './index'
 
 describe('password validator', () => {
-
+    describe('valid password', () => {
+        it(`should return an object with the following properties:
+        - result: boolean;
+        - errors: []
+        `, () => {
+            expect(passwordValidator('abc123')).toStrictEqual({result: true, errors: []})
+        })
+    })
 })
 
---
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
@@ -0,0 +1,3 @@
+export function passwordValidator(password: string) {
+    return {result: true, errors: []}
+} 
