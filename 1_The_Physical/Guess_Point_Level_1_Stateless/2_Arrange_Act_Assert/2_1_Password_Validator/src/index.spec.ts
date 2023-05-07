88cf9f13e87d91a427188dd1beddb9a2979149e2
calsilva <ka.renner@hotmail.com>
2023-05-07 17:01:43 -0300

    GREEN: return all possible errors within password

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -4,7 +4,7 @@ describe('password validator', () => {
     describe('invalid passwords', () => {   
         it('should return an object containing result:false and an array specifying the invalid length error', () => {
                 //arrange
-                const password = '1234';
+                const password = 'A234';
 
                 //act
                 const returnObject = PasswordValidator.validate(password);
@@ -18,7 +18,7 @@ describe('password validator', () => {
         
         it('should return an object containing result:false and an array specifying the digit missing error', () => {
                 //arrange
-                const password = 'abcdefghij';
+                const password = 'Abcdefghij';
 
                 //act
                 const returnObject = PasswordValidator.validate(password);
@@ -30,7 +30,7 @@ describe('password validator', () => {
                 expect(returnObject.errors[0].type).toBe('Digit missing')
         })
 
-        it('should return an object containing result:false and an array specifying the upper case letter missing error', () => {
+        it('should return an object containing result:false and an array specifying the uppercase letter missing error', () => {
                 //arrange
                 const password = 'abcdefghij1';
 
@@ -42,6 +42,24 @@ describe('password validator', () => {
                 expect(returnObject.errors.length).toBe(1)
                 expect(returnObject.errors[0].message).toBe('Password must have at least 1 uppercase letter')
                 expect(returnObject.errors[0].type).toBe('Uppercase letter missing')
+        })
+
+        it(`should return an object containing result:false and an array specifying all password errors:`, () => {
+                //arrange
+                const password = 'abc';
+
+                //act
+                const returnObject = PasswordValidator.validate(password);
+
+                //assert
+                expect(returnObject.result).toBe(false);
+                expect(returnObject.errors.length).toBe(3)
+                expect(returnObject.errors[0].message).toBe('Password must be within 5 to 15 characters long')
+                expect(returnObject.errors[0].type).toBe('Invalid length')
+                expect(returnObject.errors[1].message).toBe('Password must have at least 1 digit')
+                expect(returnObject.errors[1].type).toBe('Digit missing')
+                expect(returnObject.errors[2].message).toBe('Password must have at least 1 uppercase letter')
+                expect(returnObject.errors[2].type).toBe('Uppercase letter missing')
         })
     })
 })
--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
@@ -1,33 +1,31 @@
+import { error } from "console";
+
+export type PasswordValidationErrors = {
+    type: string;
+    message: string;
+}
+export type PasswordValidationReturn = {
+    result: boolean;
+    errors: PasswordValidationErrors[]
+}
+
 export class PasswordValidator {
-    static validate(password: string) {
+
+    static validate(password: string): PasswordValidationReturn {
+        const errors: PasswordValidationErrors[] = []
         if (password.length < 5) {
-            return {
-                result: false,
-                errors: [
-                    { type: 'Invalid length', message: 'Password must be within 5 to 15 characters long' },
-                ]
-            }
-        } else if (!/\d/.test(password)) {
-            return {
-                result: false,
-                errors: [
-                    { type: 'Digit missing', message: 'Password must have at least 1 digit' },
-                ]
-            }
-        } else if (!/[A-Z]/.test(password)) {
-            return {
-                result: false,
-                errors: [
-                    { type: 'Uppercase letter missing', message: 'Password must have at least 1 uppercase letter' },
-                ]
-            }
+            errors.push({ type: 'Invalid length', message: 'Password must be within 5 to 15 characters long' })
         }
-        
-        else {
-            return {
-                result: true,
-                errors: []
-            };
+        if (!/\d/.test(password)) {
+            errors.push({ type: 'Digit missing', message: 'Password must have at least 1 digit' })
         }
+        if (!/[A-Z]/.test(password)) {
+            errors.push({ type: 'Uppercase letter missing', message: 'Password must have at least 1 uppercase letter' })
+        }
+
+        return {
+            result: errors.length === 0,
+            errors: errors
+        };
     }
 }
