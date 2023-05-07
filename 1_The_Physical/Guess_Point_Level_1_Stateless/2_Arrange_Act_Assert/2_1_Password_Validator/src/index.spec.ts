44cef079f7b4787c5e5cb9df7e4aabfd1e17aea1
calsilva <ka.renner@hotmail.com>
2023-05-07 15:11:01 -0300

    NEW: design api responde

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -1,5 +1,17 @@
 
 describe('password validator', () => {
+    let returnObject = {
+        result: false,
+        errors: [
+            { type: 'Invalid length', message: 'Password must be within 5 to 15 characters long' },
+        ]
+    }
 
+    it('should return an object containing result:false and an array specifying the invalid length error', () => {
+    //assert
+    expect(returnObject.result).toBe(false);
+    expect(returnObject.errors.length).toBe(1)
+    expect(returnObject.errors[0].message).toBe('Password must be within 5 to 15 characters long')
+    })
 })
 
