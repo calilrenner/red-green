6f9634ac847744b2861e41264f851d7cc190a863
calsilva <ka.renner@hotmail.com>
2023-05-27 14:50:54 -0300

    [NEW]: should generate the fibonacci number for 3 position

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,11 +1,10 @@
-import {FibonacciNumber} from './index'
+import {FibonacciGenerator} from './index'
 
-describe('FibonacciNumber', () => {
+describe('FibonacciGenerator', () => {
     describe('Given that a positivie integer number has been passed in', () => {
-        it('should generate the fibonacci number for 3 position', () => {
-            const position: number = 3;
-            const fibonacciNumber = FibonacciNumber.inPosition(position);
-            expect(fibonacciNumber).toBe(2)
+        it.each([[3, 2], [0, 0], [8, 21]])('should generate the fibonacci number for %s position', (position, number) => {
+            const fibonacciNumber = FibonacciGenerator.getNumber(position);
+            expect(fibonacciNumber).toBe(number)
         })
     })
 })
--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
@@ -1,5 +1,20 @@
-export class FibonacciNumber {
-    static inPosition(index: number) {
-        return 2;
+export class FibonacciGenerator {
+  static getNumber(position: number) {
+    if (position < 0) {
+      throw new Error('Position should be zero or a positive integer');
     }
+    if (position === 0) return 0;
+    if (position === 1) return 1;
+
+    let a = 0;
+    let b = 1;
+
+    for (let i = 2; i <= position; i++) {
+      const temp = a + b;
+      a = b;
+      b = temp;
+    }
+
+    return b;
+  }
 }
